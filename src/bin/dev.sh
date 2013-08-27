4599051f34dae4650636f6e248628c35ff5436da
MaxSvargal <maxsvargal@gmail.com>
2013-08-27 05:16:35 +0400

    Fix of mongo start process and separate node and grunt processes

--- src/bin/dev.sh
+++ src/bin/dev.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-echo "\033[32m > Start MongoDB process..."
+echo "\033[32m > Start MongoDB..."
 echo "\033[0m"
-mongod --dbpath ./data/db &
-echo "\033[32m > Start Node.js process..."
+mongod --dbpath ./data/db --logpath ./data/db/mongo.log &
+echo "\033[32m > Start Node.js..."
 echo "\033[0m"
-grunt
+NODE_ENV=development ./node_modules/node-dev/bin/node-dev server &
+echo "\033[32m > Start Grunt..."
+echo "\033[0m"
+node -e "require('grunt').cli();" 
