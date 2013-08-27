630b9ce669a62bff7d52bedff2de7e5783deaab1
MaxSvargal <maxsvargal@gmail.com>
2013-08-27 04:37:18 +0400

    Async start mongo and node processes

--- src/bin/dev.sh
+++ src/bin/dev.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-cd ../
 echo "\033[32m > Start MongoDB process..."
 echo "\033[0m"
-mongod --dbpath ./data/db
+mongod --dbpath ./data/db &
 echo "\033[32m > Start Node.js process..."
 echo "\033[0m"
 grunt
